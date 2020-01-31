c1a64d44fb045ef0694a20cf906c29415d5ec95f
dar55 <dar55@yandex.ru>
2020-01-31 12:29:52 +0200

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject7")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("994a7d11-e415-4b0b-9fae-7826664d1c93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Square;
+
+namespace UnitTestProject7
+{
+    [TestClass]
+    public class SquareTest
+    {
+        Calc c = new Calc();
+        [TestMethod]
+            public void TestMethodCircle()
+            {
+                double s = c.SquareCircle(10);
+             
+            Assert.AreEqual(314.159265, Math.Round(s, 6));
+            }
+            [TestMethod]
+            public void TestMethodTr()
+            {
+            Calc.Triple s = new Calc.Triple();
+            s = c.SquareRect(2,3,4);
+            Assert.AreEqual(2.9047, Math.Round(s.squareTr, 4));
+            }
+        [TestMethod]
+        public void TestSquareRect()
+        {
+            Calc.Triple s = new Calc.Triple();
+             s = c.SquareRect(4,5,3);
+            Assert.AreEqual("прямоугольный треугольник", s.isRectangular);
+        }
+    }
+    }
+
